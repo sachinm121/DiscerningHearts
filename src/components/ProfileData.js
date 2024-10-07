df814441f549390fd66f104e9129a688ac19e7f9
Sachinm121 <sachinmahor685@gmail.com>
2024-10-07 22:44:22 +0530

    updated all profile

--- src/components/ProfileData.js
+++ src/components/ProfileData.js
@@ -1,7 +1,7 @@
 const ProfileData1 = [
   {
     image:
-      "https://discerninghearts.my.canva.site/dh-about/images/4d6ec93c9c4c7b5f8cab4fe740df080c.png",
+      "https://discerninghearts.my.canva.site/dh-about/media/4f0e087404ff828020212860b870f247.png",
     name: "Sheetal Jain",
     introduction: "I am passionate about sharing my knowledge in computer science and hail ​from Agra, known for the Taj Mahal. I completed my Bachelor of Computer ​Applications (BCA) at Raja Balawant Singh College, Khandari, Agra, and am ​currently pursuing a Master of Computer Applications (MCA) in Artificial ​Intelligence and Machine Learning at LNCT University, Bhopal. I was ​selected for a System Engineer role at HCL after getting graduated and ​have performed well in various prestigious entrance exams, including ​achieving a 93%tile in the Quantitative Section of CAT 2022, a 90%tile in ​XAT 2024, AIR 816 in CMAT 2022, AIR 1064 in NIMCET 2023, and a ​96.73%tile in MBA MH-CET 2022. I actively explore emerging fields such as ​Machine Learning, Deep Learning, Generative AI, Blockchain Technology, ​Big Data, and Data Science. In my spare time, I enjoy public speaking, ​reading, writing articles, and solving logical puzzles.",
     linked : "https://www.linkedin.com/in/sheetal-jain-0030001b5/"
@@ -15,21 +15,21 @@ const ProfileData1 = [
   },
   {
     image:
-      "https://discerninghearts.my.canva.site/dh-about/images/cf6854daa15aae0780015f3743dcf6ee.png",
+      "https://discerninghearts.my.canva.site/dh-about/media/8dc3b0a5d47f4cbeca6b36d701ab0a1b.png",
     name: "Vishnu Pratap",
     introduction: "My name is Vishnu Pratap Singh, and I am currently ​pursuing a Master of Computer Applications (MCA) at ​LNCT University in Bhopal. I hold a Bachelor of ​Science (BSc) degree from Awadh University, where I ​developed a strong foundation in mathematics. I have ​a keen interest in technology and its practical ​applications in the real world. In my leisure time, I ​enjoy gardening and engaging in physical activities ​such as walking. My academic journey, coupled with ​my personal interests, demonstrates my commitment ​to continuous learning and maintaining a well-rounded ​lifestyle.",
     linked : "https://www.linkedin.com/in/vishnu-pratap-singh-958b2a2a3/?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app"
   },
   {
     image:
-      "https://discerninghearts.my.canva.site/dh-about/images/72cdcc5da06be401ca7085f1159f1523.png",
+      "https://discerninghearts.my.canva.site/dh-about/media/c17b44a2aa58c2da9a13ea48a0f8c02c.png",
     name: "Parveen Firoz",
     introduction: "I am Parveen Firoz, a self-taught fashion designer from ​Agra. I hold a Bachelor's degree in Computer Applications ​(BCA) from Raja Balwant Singh College, Agra. After ​completing my degree, I realized that the technical field ​did not align with my true interests. My passion lies in ​creativity, which led me to pursue fashion design.Currently, I am learning the intricacies of dress design, ​garment construction, and sewing techniques. I rely on ​platforms such as YouTube and Google as my primary ​learning resources. I also enjoy sketching, creating ​patterns, and honing my sewing skills.",
     linked : ""
   },
   {
     image:
-      "https://discerninghearts.my.canva.site/dh-about/media/f51bca5d9dc239c59cc7f1a2f06f264b.jpg",
+      "https://discerninghearts.my.canva.site/dh-about/media/bd7d0aac1ec3397d075ed11f38a451b7.png",
     name: "Kavita Dwivedi",
     introduction: "My name is Kavita Dwivedi and I am currently pursuing MSc in ​Mathematics from Agra college of Dr.Bhim Rao Ambedkar University ​where I also completed my graduation in 2022. Coming from a ​middle-class background, I have always embraced the opportunity ​to explore various fields, which has shaped me into a versatile and ​adaptive learner. While my academic journey has been rooted in ​mathematics, my curiosity has led me to gain knowledge and ​experience beyond a single domain. Teaching is another passion of ​mine, as it allows me to connect with students and share the insights ​I have gained throughout my diverse experiences. I believe that ​continuous learning and helping others to grow are key to personal ​development. I am always excited about new challenges and look ​forward to contributing meaningfully to whichever field I ​step into next.",
     linked : ""
